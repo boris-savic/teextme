8ad0ca5938efa2a973951b72dc1efe75dd901b98
Luka Zakrajšek <luka@bancek.net>
2013-03-30 16:08:04 +0100

    Setup logging.

--- teextme/settings.py
+++ teextme/settings.py
@@ -145,11 +145,24 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
         }
     },
     'loggers': {
@@ -158,6 +171,11 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+        'teextme': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
     }
 }
 
