ad29be3f32caf83aa9f71178d9dd13edde34b77d
kostans3k <blaz.kostanjsek@gmail.com>
2013-04-01 08:27:17 -0700

    Contacts and Messaging views.

---
+++ contacts/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from contacts.models import *
+
+class ContactForm(ModelForm):
+  
+	class Meta:
+		model = Contact
+		exclude = ('user', 'contact_user')
+	
+	def __init__(self, user, *args, **kwargs):
+		self.user = user
+		super(ContactForm, self).__init__(*args, **kwargs)
+	
+	def save(self, commit=True):
+		instance = super(ContactForm, self).save(commit=False)
+		if self.user:
+				instance.user = self.user
+				instance.save()
+		return instance
